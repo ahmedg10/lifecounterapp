e6c28e073e421703c0ef0c244b2b86eea222aee9
ahmedg10 <a.ghaddah@gmail.com>
2024-04-25 11:55:19 -0700

    broke working version one step closer to refixing when trying to teach myself nibs

--- lifecounter/lifecounter/PlayerTableViewCell.swift
+++ lifecounter/lifecounter/PlayerTableViewCell.swift
@@ -6,59 +6,85 @@
 //
 import UIKit
 
-class PlayerTableViewCell: UITableViewCell, UITextFieldDelegate{
+import UIKit
 
-    @IBOutlet weak var playerNameLabel: UILabel!
+class PlayerTableViewCell: UITableViewCell {
+
+    static let identifier = "PlayerTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "PlayerTableViewCell", bundle: nil)
+    }
+
     @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var lifeLabel: UILabel!
     @IBOutlet weak var addLifeTextField: UITextField!
     @IBOutlet weak var subtractLifeTextField: UITextField!
-    @IBOutlet weak var lifeLabel: UILabel!
     
-    // Assuming you have a delegate protocol for cell actions
+
+    @IBOutlet weak var minusButton: UIButton!
+    
     weak var delegate: PlayerTableViewCellDelegate?
-       
-       override func awakeFromNib() {
-           super.awakeFromNib()
-           // Optionally set the keyboard type for the life text fields to number pad.
-           addLifeTextField.keyboardType = .numberPad
-           subtractLifeTextField.keyboardType = .numberPad
-       }
-       
-    func configure(with player: Player, atIndex index: Int) {
-        playerNameLabel.text = "Player \(index + 1): \(player.name)"
-        lifeLabel.text = "\(player.lifeTotal) Lives"
+    
+    override func awakeFromNib() {
+            super.awakeFromNib()
+            
+            // Set the keyboard type to number pad for the text fields
+            addLifeTextField.keyboardType = .numberPad
+            subtractLifeTextField.keyboardType = .numberPad
+
+            // Setup cell UI based on the storyboard configuration
+            setupCellAppearance()
+        }
+    
+    private func setupCellAppearance() {
+            // Set the colors and other UI elements here to match the storyboard design
+            // For example, if you have set specific colors for your buttons in the storyboard:
+            plusButton.backgroundColor = .systemGreen
+            minusButton.backgroundColor = .systemRed
+            
+            // Set up label colors if needed
+            playerNameLabel.textColor = .black
+            lifeLabel.textColor = .black
+            
+            // Set any border, corner radius, or other visual properties
+            plusButton.layer.cornerRadius = plusButton.bounds.height / 2
+            minusButton.layer.cornerRadius = minusButton.bounds.height / 2
+        }
+
+
+
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        if let amountText = addLifeTextField.text, let amount = Int(amountText), amount > 0 {
+            delegate?.addLifeToPlayer(amount: amount, inCell: self)
+        }
     }
-       
+    
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        if let amountText = subtractLifeTextField.text, let amount = Int(amountText), amount > 0 {
+            delegate?.subtractLifeFromOthers(amount: amount, inCell: self)
+        }
+    }
+    
+    func configure(with player: Player, atIndex index: Int) {
+        playerNameLabel.text = "Player \(index + 1)"
+        lifeLabel.text = "\(player.lifeTotal) Lives"
+        
+        setupCellAppearance()
 
-       // Plus button action
-       @IBAction func plusButtonTapped(_ sender: UIButton) {
-           if let amountText = addLifeTextField.text, let amount = Int(amountText), amount > 0 {
-               delegate?.addLifeToPlayer(amount: amount, inCell: self)
-           }
-       }
-       
-       // Minus button action
-       @IBAction func minusButtonTapped(_ sender: UIButton) {
-           if let amountText = subtractLifeTextField.text, let amount = Int(amountText), amount > 0 {
-               delegate?.subtractLifeFromOthers(amount: amount, inCell: self)
-           }
-       }
-       
-       // Prepare for cell reuse
-       override func prepareForReuse() {
-           super.prepareForReuse()
-           // Reset the cell to its default state
-           playerNameLabel.text = "New Player"
-           lifeLabel.text = "20"
-           addLifeTextField.text = ""
-           subtractLifeTextField.text = ""
-       }
-   }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        playerNameLabel.text = "New Player"
+        lifeLabel.text = "20 Lives"
+        addLifeTextField.text = ""
+        subtractLifeTextField.text = ""
+    }
+}
 
-   // Define the delegate protocol that the ViewController will conform to.
-   protocol PlayerTableViewCellDelegate: AnyObject {
-       func addLifeToPlayer(amount: Int, inCell cell: PlayerTableViewCell)
-       func subtractLifeFromOthers(amount: Int, inCell cell: PlayerTableViewCell)
-       func changePlayerName(newName: String, inCell cell: PlayerTableViewCell)
-   }
+protocol PlayerTableViewCellDelegate: AnyObject {
+    func addLifeToPlayer(amount: Int, inCell cell: PlayerTableViewCell)
+    func subtractLifeFromOthers(amount: Int, inCell cell: PlayerTableViewCell)
+}
--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -6,90 +6,50 @@
 //
 
 import UIKit
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, PlayerTableViewCellDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, PlayerTableViewCellDelegate {
+
     @IBOutlet weak var tableView: UITableView!
     var players: [Player] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(PlayerTableViewCell.nib(), forCellReuseIdentifier: PlayerTableViewCell.identifier)
         tableView.dataSource = self
         tableView.delegate = self
         initializePlayers()
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        view.addGestureRecognizer(tapGesture)
-        
-        // Register the nib file for the cell
-        let nib = UINib(nibName: "PlayerTableViewCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "PlayerCell")
     }
     
     func initializePlayers() {
-        for index in 1...2 {  // For example, start with 2 players
-            let newPlayerName = "Player \(index)"
-            players.append(Player(name: newPlayerName))
+        for index in 1...4 {  // Example to start with 4 players
+            players.append(Player(name: "Player \(index)", lifeTotal: 20))
         }
-        tableView.reloadData()
-    }
-    
-    @IBAction func addPlayerButtonTapped(_ sender: Any) {
-        let newPlayerName = "Player \(players.count + 1)"
-            players.append(Player(name: newPlayerName))
-            let newIndexPath = IndexPath(row: players.count - 1, section: 0)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
     }
 
-
-    @objc func hideKeyboard() {
-        view.endEditing(true)
-    }
-
-}
-
-extension ViewController {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return players.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath) as? PlayerTableViewCell else {
-            fatalError("Cell with identifier 'PlayerCell' not found")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PlayerTableViewCell.identifier, for: indexPath) as? PlayerTableViewCell else {
+            fatalError("Cell with identifier \(PlayerTableViewCell.identifier) not found")
         }
-        
-        var player = players[indexPath.row]
-        // Configure the cell with data from the players array
-        if player.name == "Player" {
-            // If the player name is just "Player", append the number
-            player.name += " \(indexPath.row + 1)"
-        }
+        let player = players[indexPath.row]
         cell.configure(with: player, atIndex: indexPath.row)
         cell.delegate = self
-        print("Setting delegate for cell at row \(indexPath.row)")
-
         return cell
     }
-}
 
-
-extension ViewController {
     func addLifeToPlayer(amount: Int, inCell cell: PlayerTableViewCell) {
         guard let indexPath = tableView.indexPath(for: cell) else { return }
         players[indexPath.row].lifeTotal += amount
-        tableView.reloadRows(at: [indexPath], with: .automatic)
+        tableView.reloadRows(at: [indexPath], with: .none)
     }
     
     func subtractLifeFromOthers(amount: Int, inCell cell: PlayerTableViewCell) {
         guard let indexPath = tableView.indexPath(for: cell) else { return }
-        for (index, player) in players.enumerated() where index != indexPath.row {
-            player.lifeTotal -= amount
+        for (index, _) in players.enumerated() where index != indexPath.row {
+            players[index].lifeTotal -= amount
         }
         tableView.reloadData()
     }
-    
-    func changePlayerName(newName: String, inCell cell: PlayerTableViewCell) {
-        guard let indexPath = tableView.indexPath(for: cell), !newName.isEmpty else { return }
-        players[indexPath.row].name = newName
-        tableView.reloadRows(at: [indexPath], with: .none)
-    }
 }
-
-
